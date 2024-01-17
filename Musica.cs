1758069e2eebeedcfdfd15a97d74671817eb6f58
Fabiana Mendes <feelpitymendes@gmail.com>
2024-01-17 20:41:07 -0300

    CHANGES

---
+++ Musica.cs
@@ -0,0 +1,8 @@
+﻿class Musica
+{
+    //atribuindo nome a classe Musica
+   public string nome;
+   public string artista;
+   public int duracao;
+   public bool disponivel;
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿//criação de objeto
+//var tipo Musica e seu objeto 
+Musica musica1 = new Musica();
+//acessando a variável para colocar o nome
+musica1.nome = "Ditto";
+musica1.artista = "New Jeans";
